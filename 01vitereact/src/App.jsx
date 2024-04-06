19b34ddbefc6f5fb5c83d902315f868896168201
rekhaaryal <aryalr458@gmail.com>
2024-04-06 09:10:32 +0545

    building a 01basicReact and 01 viteReact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Home from "./home"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Home/>
+    <h1>hello i am homeless</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/home.jsx
@@ -0,0 +1,9 @@
+
+
+
+function Home(){
+    return(
+            <h2>welcome to home</h2>
+        )
+}
+export default Home
